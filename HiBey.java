357ae946ca2b14e82e96e1ad6e7adc406860d0e7
Noura Said AL Saidi <97401813+NouraSaid@users.noreply.github.com>
2024-02-19 10:24:05 +0400

    Update CombineTwoStringAsMentioned.java

--- HiBey.java
+++ HiBey.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-            String result = Order("Hi", "Bye");
+            String result = orderMentioned("Hi", "Bye");
             System.out.println(result);
         }
 
-        public static String Order(String a, String b) {
+        public static String orderMentioned(String a, String b) {
             return a + b + b + a;
         }
     }
